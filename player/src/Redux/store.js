d23fd24e1c1f8f46c4e11c8990f9186f2324106d
Ashish Kumar <112822104+ashishsen003@users.noreply.github.com>
2024-03-11 18:21:28 +0530

    Delete player directory

--- player/src/Redux/store.js
+++
@@ -1,5 +0,0 @@
-import { reducer } from "./videos/reducer"
-import {legacy_createStore, applyMiddleware} from 'redux'
-import {thunk} from 'redux-thunk'
-
-export const store = legacy_createStore(reducer, applyMiddleware(thunk))
